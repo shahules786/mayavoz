6c4bced3607f23db76d911f9a3471de22db53fa1
shahules786 <shahules786@gmail.com>
2022-10-05 15:20:43 +0530

    reformat utils

--- enhancer/utils/utils.py
+++ enhancer/utils/utils.py
@@ -1,19 +1,24 @@
-
 import os
 from typing import Optional
 from enhancer.utils.config import Files
 
-def check_files(root_dir:str, files:Files):
 
-    path_variables = [member_var for member_var in dir(files) if not member_var.startswith('__')]
+def check_files(root_dir: str, files: Files):
+
+    path_variables = [
+        member_var
+        for member_var in dir(files)
+        if not member_var.startswith("__")
+    ]
     for variable in path_variables:
-        path = getattr(files,variable)
-        if not os.path.isdir(os.path.join(root_dir,path)):
+        path = getattr(files, variable)
+        if not os.path.isdir(os.path.join(root_dir, path)):
             raise ValueError(f"Invalid {path}, is not a directory")
-            
-    return files,root_dir
 
-def merge_dict(default_dict:dict, custom:Optional[dict]=None):
+    return files, root_dir
+
+
+def merge_dict(default_dict: dict, custom: Optional[dict] = None):
     params = dict(default_dict)
     if custom:
         params.update(custom)
