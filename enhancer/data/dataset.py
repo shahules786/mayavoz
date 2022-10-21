178a4523ef06e272128e431d9d8adad1fa1c7592
shahules786 <shahules786@gmail.com>
2022-10-21 09:48:28 +0530

    fix worker init fn

--- enhancer/data/dataset.py
+++ enhancer/data/dataset.py
@@ -189,8 +189,8 @@ class TaskDataset(pl.LightningDataModule):
         worker_info = torch.utils.data.get_worker_info()
         dataset = worker_info.dataset
         worker_id = worker_info.id
-        split_size = len(dataset.data) // worker_info.num_workers
-        dataset.data = dataset.data[
+        split_size = len(dataset.dataset.train_data) // worker_info.num_workers
+        dataset.data = dataset.dataset.train_data[
             worker_id * split_size : (worker_id + 1) * split_size
         ]
 
