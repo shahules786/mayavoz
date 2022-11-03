e0abb458d5a17d3642ffa8c676459da4f19d8357
shahules786 <shahules786@gmail.com>
2022-11-03 10:40:22 +0530

    15hrs

--- hpc_entrypoint.sh
+++ hpc_entrypoint.sh
@@ -38,12 +38,12 @@ pwd
 # rm -rf /scratch/c.sistc3/MS-SNSD/DNS30/NoisySpeech_testing
 # rm -rf /scratch/c.sistc3/MS-SNSD/DNS30/CleanSpeech_testing
 
-cp -r /scratch/c.sistc3/MS-SNSD/DNS30/NoisySpeech_testing /scratch/c.sistc3/MS-SNSD/DNS15/
-cp -r /scratch/c.sistc3/MS-SNSD/DNS30/CleanSpeech_testing /scratch/c.sistc3/MS-SNSD/DNS15/
+# cp -r /scratch/c.sistc3/MS-SNSD/DNS30/NoisySpeech_testing /scratch/c.sistc3/MS-SNSD/DNS15/
+# cp -r /scratch/c.sistc3/MS-SNSD/DNS30/CleanSpeech_testing /scratch/c.sistc3/MS-SNSD/DNS15/
 
 python noisyspeech_synthesizer.py
 
-mv ./CleanSpeech_testing/ /scratch/c.sistc3/MS-SNSD/DNS15
-mv ./NoisySpeech_testing/ /scratch/c.sistc3/MS-SNSD/DNS15
+mv ./CleanSpeech_training/ /scratch/c.sistc3/MS-SNSD/DNS15
+mv ./NoisySpeech_training/ /scratch/c.sistc3/MS-SNSD/DNS15
 ls /scratch/c.sistc3/MS-SNSD/DNS15
 #python enhancer/cli/train.py
