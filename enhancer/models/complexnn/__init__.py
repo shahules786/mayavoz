70d17f6586eb2d03b69c71f76962f0265519b0ef
shahules786 <shahules786@gmail.com>
2022-11-05 16:59:04 +0530

    add imports

--- enhancer/models/complexnn/__init__.py
+++ enhancer/models/complexnn/__init__.py
@@ -2,3 +2,4 @@ from enhancer.models.complexnn.conv import ComplexConv2d  # noqa
 from enhancer.models.complexnn.conv import ComplexConvTranspose2d  # noqa
 from enhancer.models.complexnn.rnn import ComplexLSTM  # noqa
 from enhancer.models.complexnn.utils import ComplexBatchNorm2D  # noqa
+from enhancer.models.complexnn.utils import ComplexRelu  # noqa
